899d4f4bcc74b156d2ecab7532e81a8151696b0b
imad <imadamroug89@outlook.com>
2025-11-28 01:16:23 +0100

    Initial commit met HTML pagina & Spring Boot setup

---
+++ src/main/java/org/imadsnetwork/fsmgnt/FsManagementApplication.java
@@ -0,0 +1,12 @@
+package org.imadsnetwork.fsmgnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FsManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FsManagementApplication.class, args);
+    }
+}
---
+++ src/test/java/org/imadsnetwork/fsmgnt/FsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package org.imadsnetwork.fsmgnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
