a5d2b9af9bbd47f240eb64d67a47734d895d738b
imad <imadamroug89@outlook.com>
2025-11-29 11:07:32 +0100

    Added support for drive (Windows) & Volume (macOS) recognition

--- src/main/java/org/imadsnetwork/fsmgnt/HomeController.java
+++ src/main/java/org/imadsnetwork/fsmgnt/HomeController.java
@@ -1,11 +1,52 @@
 package org.imadsnetwork.fsmgnt;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.File;
+import java.util.Arrays;
 
 @Controller //Vertelt java om dit klasse gebruikt moet worden als Controller
 public class HomeController {
-    @RequestMapping("/")
-    public String Index(){
-        return "index.html";
+
+    @GetMapping("/drives/")
+    @ResponseBody
+    public File[] getDrives() {
+        /*
+            Haalt alle schijf letters (Windows) en zet deze in een array
+            macOS haalt alle "volumes" (in "/Volumes") op en zet die in hetzelfde array
+        */
+
+        String os = System.getProperty("os.name").toLowerCase();
+
+        if (os.contains("win")) {
+            return File.listRoots();
+        }
+
+        if (os.contains("mac")) {
+            File volumes = new File("/Volumes");
+            File[] allDrives = volumes.listFiles();
+
+            if (allDrives == null) {
+                return new File[0];
+            }
+
+            return Arrays.stream(allDrives)
+                    .filter(f -> !f.getName().contains("hidden"))
+                    .toArray(File[]::new);
+        }
+
+
+        // Mocht het OS toch niet ondersteund worden
+        return new File[0];
+    }
+
+
+    @GetMapping("/path/root/{*rest}")
+    @ResponseBody
+    public String handle(@PathVariable String rest) {
+        return rest;
     }
 }
+
