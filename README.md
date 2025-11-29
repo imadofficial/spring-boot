71825191bee625630c3243b196446416bdeba794
imad <imadamroug89@outlook.com>
2025-11-29 15:31:49 +0100

    Added README & optimized my code

---
+++ README.md
@@ -0,0 +1,9 @@
+# A Web Based file explorer in Spring Boot
+This is a schoolproject meant to demonstrate our ability to in a short period of time (48hrs) allow us to create something we haven't used before. My choice was Spring Boot (for the API) & a NextJS frontend.
+
+## How do I run this?
+There are 2 repositories available:
+    - imadofficial/spring-boot
+    - imadofficial/web-file-manager
+
+You need these 2 repositories cloned to be able to run it.
--- src/main/java/org/imadsnetwork/fsmgnt/HomeController.java
+++ src/main/java/org/imadsnetwork/fsmgnt/HomeController.java
@@ -1,13 +1,9 @@
 package org.imadsnetwork.fsmgnt;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.Map;
+import java.util.*;
 
 @Controller //Vertelt java om dit klasse gebruikt moet worden als Controller
 public class HomeController {
